ba06fcaf5ef6f42c966af2d6c11c0b728e94ca79
soomin9106 <soomin9106@naver.com>
2024-03-15 16:43:45 +0900

    [Settings] responsive Footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
